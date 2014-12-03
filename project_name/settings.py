67c25d85208e5962d453330d65ab3fd90086c53a
Guilherme Louro <guilherme-louro@hotmail.com>
2014-12-03 11:23:11 -0200

    Config template path + urls

--- project_name/settings.py
+++ project_name/settings.py
@@ -76,6 +76,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Template path
+TEMPLATE_DIRS = (
+    BASE_DIR.parent.child('templates'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
--- project_name/urls.py
+++ project_name/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'', include('core.urls', namespace='core')),
 )
