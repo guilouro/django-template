c91bfcc621149673419fa1c84b791e4389dd71e8
Guilherme Louro <guilherme-louro@hotmail.com>
2015-01-20 17:58:34 -0200

    TEMPLATE_DEBUG is DEBUG

--- project_name/settings.py
+++ project_name/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '{{ secret_key }}'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
