43d1f15a7912aa98a3735d2ccf7c1590add04b9c
Guilherme Louro <guilherme-louro@hotmail.com>
2015-08-17 23:00:55 -0300

    middleware htmlmin

--- project_name/settings.py
+++ project_name/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
+    'htmlmin.middleware.MarkRequestMiddleware',
 )
 
 ROOT_URLCONF = '{{ project_name }}.urls'
